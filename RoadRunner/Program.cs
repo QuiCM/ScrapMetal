459e3e66d5905fbf8d325206e1551ff0fc5894d5
Chris <2648373+QuiCM@users.noreply.github.com>
2021-06-30 20:58:12 +0930

    Add better commentary & flow to RoadRunner

--- RoadRunner/Program.cs
+++ RoadRunner/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics;
 using System.Threading.Tasks;
-using CandyBar.models;
 using ScrapMetal;
 
 /*
@@ -20,46 +19,49 @@ namespace RoadRunner
                 authToken = args[0];
             }
 
-            ulong permissions = (ulong)(permission_bitflags.MANAGE_CHANNELS | permission_bitflags.ADD_REACTIONS | permission_bitflags.VIEW_CHANNEL | permission_bitflags.SEND_MESSAGES
-                    | permission_bitflags.EMBED_LINKS | permission_bitflags.ATTACH_FILES | permission_bitflags.READ_MESSAGE_HISTORY | permission_bitflags.USE_EXTERNAL_EMOJIS
-                    | permission_bitflags.USE_SLASH_COMMANDS | permission_bitflags.USE_PUBLIC_THREADS);
-            Debug.WriteLine($"Join URL: https://discord.com/oauth2/authorize?client_id=<client id>&scope=bot&permissions={permissions}");
-
-            Debug.WriteLine($"Shoelaces found: {authToken}");
+            Debug.WriteLine($"Using commandline auth token: {authToken}");
 
             await StayConnectedAsync(authToken);
 
-            Debug.WriteLine("Expected exit point reached.");
+            Debug.WriteLine("RoadRunner disappearing into the sunset.");
+        }
+
+        static async Task StayConnectedAsync(string auth, ScrapMetalBrain savedBrain = null)
+        {
+            //Attempt to connect, retrieving the brain once the connection ends
+            ScrapMetalBrain brain = await ConnectAsync(auth, savedBrain);
+
+            // According to Discord docs: bots should wait 3-5 seconds before trying to reconnect in the event of a disconnect
+            // Also this gives plenty of time for cancellations to propagate
+            Debug.WriteLine("Waiting for ScrapMetalBot to rust away.");
+            await Task.Delay(3000);
+
+            Debug.WriteLine("Building a new ScrapMetalBot!");
+            // Reconnect with the same brain
+            await StayConnectedAsync(auth, brain);
         }
 
         static async Task<ScrapMetalBrain> ConnectAsync(string auth, ScrapMetalBrain savedBrain = null)
         {
+            // Build a new bot
             using ScrapMetalBot scrapMetal = new ScrapMetalBuilder()
                                                   .WithAuth(auth)
                                                   .WithBrain(savedBrain)
                                                   .Build();
             Debug.WriteLine("RoadRunner has built itself a ScrapMetalBot. Pressing play now!");
 
+            // Connect and poll. PollAsync will return once the bot has disconnected
             await scrapMetal.ConnectAsync();
             await scrapMetal.PollAsync();
 
+            // At this point we have disconnected. Retrieve the brain for reuse, and flag the ScrapMetal instance for closure
+            // Technically it should already do this while disposing, but no harm in making sure
             ScrapMetalBrain brain = scrapMetal.Brain;
             scrapMetal.Close();
 
             Debug.WriteLine("ScrapMetal fell apart.");
-
+            // Pass the brain back for re-use
             return brain;
-        }
-
-        static async Task StayConnectedAsync(string auth, ScrapMetalBrain savedBrain = null)
-        {
-            ScrapMetalBrain brain = await ConnectAsync(auth, savedBrain);
-
-            Debug.WriteLine("Waiting for ScrapMetalBot to rust away.");
-            await Task.Delay(3000);
-
-            Debug.WriteLine("Building a new ScrapMetalBot!");
-            await StayConnectedAsync(auth, brain);
         }
     }
 }
